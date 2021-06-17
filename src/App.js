6d46d2d2c4b4e89566c00e8410f9775784917ab7
Ben <Ben@Bens-MacBook-Pro.local>
2021-06-17 16:38:47 -0400

    Basic App Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+import Navbar from './Components/Navbar/index.js';
+import LandingPage from './Components/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar />
+
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          {/* <Route exact path="/checklist" component={Checklist} /> */}
+          {/* <Route exact path="/map" component={Map} /> */}
+
+        </Switch>
     </div>
   );
 }
---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,23 @@
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+
+const NavigationBar = () => {
+
+    return(
+        <div>
+            <Navbar bg="light" expand="lg" sticky="top">
+                <Navbar.Brand href="/">SAFESPOT</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    )
+}
+
+export default NavigationBar;
