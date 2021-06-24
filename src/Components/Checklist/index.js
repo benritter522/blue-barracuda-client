901b4117742ce53839e22fa275454610c0463cb3
Ben <Ben@Bens-MacBook-Pro.local>
2021-06-24 13:19:54 -0400

    checklist component comments for Edgard

--- src/Components/Checklist/index.js
+++ src/Components/Checklist/index.js
@@ -1,8 +1,23 @@
+// import material UI checkbox component (don't forget to yarn add whatever you need to install)
+    // https://material-ui.com/components/checkboxes/ if you need some references.
 
 const Checklist = () => {
+
+    // array of strings? array of objects which include strings and a 'checked' boolean saved as state variables? dealer's choice.
+
     return(
         <div>
             <p>Checklist</p>
+
+            {/* 
+            
+            map through array
+            
+            return checkbox with array item contents passed through as props
+            
+                
+                not currently planning on us storing any data for the user, so the checklist will likely refresh each time the app is loaded, but honestly I don't really care about having any further functionality past that, at least not at this time.
+             */}
         </div>
     )
 }
--- src/Components/EvacuationZones/index.js
+++ src/Components/EvacuationZones/index.js
@@ -2,7 +2,7 @@
 const EvacuationZones = () => {
     return(
         <div>
-            <iframe width="90%" height="80%" frameborder="0" scrolling="no" allowfullscreen src="https://arcg.is/0q8OSP0"></iframe>
+            <iframe width="300" height="700" frameborder="0" scrolling="no" allowfullscreen src="https://arcg.is/0q8OSP0"></iframe>
         </div>
     )
 }
