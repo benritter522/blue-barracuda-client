a628e968154e3d381279801cf3cb28e85bc87b30
Ben <Ben@Bens-MacBook-Pro.local>
2021-07-06 12:13:54 -0400

    resized iframe

--- src/Components/EvacuationZones/index.js
+++ src/Components/EvacuationZones/index.js
@@ -2,7 +2,7 @@
 const EvacuationZones = () => {
     return(
         <div>
-            <iframe width="300" height="700" frameborder="0" scrolling="no" allowfullscreen title="evacuationZoneFrame" src="https://arcg.is/0q8OSP0"></iframe>
+            <iframe style={{width: '90vw', height: '70vh'}} frameborder="0" scrolling="no" allowfullscreen title="evacuationZoneFrame" src="https://arcg.is/0q8OSP0"></iframe>
             {/* sandbox attribute? very important for security. may NEED after adding user login info*/}
         </div>
     )
